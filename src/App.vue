21b74fc7592528177b6b9d6f22a7c136fce91dd9
Gene <g-nakagaki@ceres-inc.jp>
2023-10-02 14:37:39 +0900

    2nd commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import TaskList from './components/TaskList.vue'
 </script>
 
 <template>
   <main>
-    <HelloWorld />
+    <TaskList />
   </main>
 </template>
 
